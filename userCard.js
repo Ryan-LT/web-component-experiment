4e8da6e1508afbbad1444471a3f8ad3fb0cd464b
Ryan <54348153+thinhlesdev@users.noreply.github.com>
2021-05-30 19:26:26 +0700

    feat: basic attachShadow

--- userCard.js
+++ userCard.js
@@ -1,9 +1,23 @@
+const template = document.createElement('template');
+template.innerHTML = `
+    <style>
+        h1 {
+            color: red;
+        }
+    </style>
+    <div class="user-card">
+    <h1></h1>
+    </div>
+`;
+
 class UserCard extends HTMLElement {
   constructor() {
     super();
-    this.innerHTML = `<h1>${this.getAttribute('name')}</h1>`;
+    this.attachShadow({ mode: 'open' });
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
+    console.log(this)
+    this.shadowRoot.querySelector('h1').innerText = this.getAttribute('name');
   }
 }
 
 window.customElements.define('user-card', UserCard);
- 
